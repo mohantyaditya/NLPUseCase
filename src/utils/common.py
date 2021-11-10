fbc1bfba239adfcd18273dbccd23f2a07e114877
mohantyaditya <aditya.mohanty601@gmail.com>
2021-11-09 22:18:59 -0800

    Stage 1 structure added

---
+++ src/utils/common.py
@@ -0,0 +1,15 @@
+
+
+import os
+import yaml 
+import logging
+import time 
+import pandas as pd
+import json
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"Yaml file {path_to_yaml} created successfully")
+
+    return content 
