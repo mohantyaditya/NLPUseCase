c49c542b39f76b13353e7373a04a6d0164e958fc
mohantyaditya <aditya.mohanty601@gmail.com>
2021-11-10 02:46:49 -0800

    Added stage 2 after creating tfidf featurizer

--- src/stage2.py
+++ src/stage2.py
@@ -2,9 +2,10 @@ import argparse
 import os
 import shutil
 from tqdm import tqdm
+from src.utils.featurize import save_matrix
 from src.utils.common import read_yaml,create_directories,get_df
 from src.utils.data_mgmt import process_posts
-from sklearn.feature_extraxtion.text import CountVectorizer, TfidfVectorizer 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer,TfidfTransformer 
 
 import logging
 import numpy as np 
@@ -30,7 +31,7 @@ def main(config_path,params_path):
     train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
     test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
 
-    featurized_data_dir_path = os.path.join(artifacts["ARTIFACT_DIR"],artifacts["PREPARED_DATA"])
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACT_DIR"],artifacts["FEATURIZED_DATA"])
     create_directories([featurized_data_dir_path])
 
     featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
@@ -43,6 +44,22 @@ def main(config_path,params_path):
     df_train = get_df(train_data_path)
 
     train_words = np.array(df_train.text.str.lower().values.astype(("U")))
+
+    bag_of_words = CountVectorizer(
+        stop_words = "english",max_features = max_features,ngram_range = (1,ngrams)
+    )
+    bag_of_words.fit(train_words)
+
+    train_words_binary_matrix = bag_of_words.transform(train_words)
+
+    tfidf = TfidfTransformer(smooth_idf = False)
+
+    tfidf.fit(train_words_binary_matrix)
+
+    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+
+    save_matrix(df_train,train_words_tfidf_matrix,featurized_train_data_path)
+
 
     #print(train_words[:20])
 
@@ -64,7 +81,7 @@ if __name__ == '__main__':
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
         main(config_path=parsed_args.config,params_path=parsed_args.param)
-        logging.info(">>>>> stage one completed! all the data are saved in local <<<<<n")
+        logging.info(">>>>> stage two completed! all the data are saved in local <<<<<n")
     except Exception as e:
         logging.exception(e)
         raise e
